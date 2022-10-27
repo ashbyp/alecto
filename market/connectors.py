1e8e4257b7cf77b398dc41955ea19ffec5010c7d
ashbyp <R0se-/:bud0>
2022-10-27 12:43:40 +0100

    added binance

--- market/connectors.py
+++ market/connectors.py
@@ -1,28 +1,38 @@
 import pprint
-from market.gateio import GateIO
-from market.coinmarketcappro import CoinmarketcapPRO
+from market.gateioapi import GateIO
+from market.coinmarketcapproapi import CoinmarketcapPRO
+from market.binanceapi import Binance
 from utils import config
+
+conf = config.get()
+gate = GateIO(conf)
+coin = CoinmarketcapPRO(conf)
+bin = Binance(conf)
+
+
+def print_last_price(ccy_pair: str) -> None:
+    g = gate.get_last_price(ccy_pair)
+    c = coin.get_last_price(ccy_pair)
+    b = bin.get_last_price(ccy_pair.replace('_', ''))
+    print(f'{ccy_pair}: gate={g}, coin={c}, binance={b}')
+
+
+def print_ticker(ccy_pair: str) -> None:
+    print('GATE')
+    print(gate.get_ticker(ccy_pair))
+    print('COIN')
+    print(coin.get_ticker(ccy_pair))
+    print('BINANCE')
+    print(bin.get_ticker(ccy_pair.replace('_', '')))
 
 
 def main():
-    conf = config.get()
-    gate = GateIO(conf)
-    coin = CoinmarketcapPRO(conf)
+    print_last_price('BTC_USDT')
+    print_last_price('ETH_USDT')
+    print_last_price('DOGE_USDT')
 
-    ccy_pair = 'BTC_USD'
-    print(f'{ccy_pair}: gate={gate.get_last_price(ccy_pair)}, coin={coin.get_last_price(ccy_pair)}')
-    ccy_pair = 'ETH_USD'
-    print(f'{ccy_pair}: gate={gate.get_last_price(ccy_pair)}, coin={coin.get_last_price(ccy_pair)}')
-    ccy_pair = 'BTC_USDT'
-    print(f'{ccy_pair}: gate={gate.get_last_price(ccy_pair)}, coin={coin.get_last_price(ccy_pair)}')
+    print_ticker('BTC_USDT')
 
-    ccy_pair = 'BTC_USD'
-    g = gate.get_ticker(ccy_pair)
-    c = coin.get_ticker(ccy_pair)
-    print(f'GATE -{type(g)}----------------------------')
-    pprint.pprint(g)
-    print(f'COIN -{type(c)}----------------------------')
-    pprint.pprint(c)
 
 
 if __name__ == '__main__':
