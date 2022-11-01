4cff17114007c22edb3ad13196aeb8038d73a96c
ashbyp <R0se-/:bud0>
2022-11-01 12:04:52 +0000

    remove hardcoded key

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='alecto',
+    version='1.0',
+    packages=['utils', 'utils.tests', 'market', 'market.providers', 'strategy', 'execution'],
+    url='',
+    license='',
+    author='ashbyp',
+    author_email='ashbyp@yahoo.co.uk',
+    description='The unceasing'
+)
